fd89db0554dc9bd540939b697ec7ff803acdcd2c
Daniel Briceno <daniel.b100726@gmail.com>
2023-02-19 16:55:29 -0400

    Class 7: useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
+// import { HttpModule, HttpService } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -8,7 +9,7 @@ const API_KEY = '123456789';
 const API_KEY_PROD = 'PRODUC15156';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule],
   controllers: [AppController],
   providers: [
     //Usa implicitamente useClass...
@@ -17,6 +18,16 @@ const API_KEY_PROD = 'PRODUC15156';
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,11 @@ import { Injectable, Inject } from '@nestjs/common';
 @Injectable()
 export class AppService {
   //Como lo que vamos a inyectar es un valor y no una clase, la pasamos en el constructor como un atributo privado, utilizamos el decorador @Inject(), eso lo podemos usar para inyectar un valor en cualquier parte donde lo necesitemos en la aplicacion...
-  constructor(@Inject('API_KEY') private apiKey: string) {}
-  getHello(): string {
-    return `Hello World! ${this.apiKey}`;
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
+  getHello(): any {
+    return { message: `Hello World! ${this.apiKey}`, tasks: this.tasks };
   }
 }
